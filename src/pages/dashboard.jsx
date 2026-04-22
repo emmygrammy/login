07ff48c37dd4213ca1cc5e1e96c7562ff637fef9
emmy grammy <ojeabuo24@gmail.com>
2026-04-22 23:55:41 +0100

    src and server

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,8 +1,14 @@
+import { Button } from './button';
+
+export { Button };
 
 export default function Dashboard() {
   return (
     <div>
       <h1>Dashboard</h1>
+      <p>Welcome to your dashboard</p>
+      <Button>Logout</Button>
+      <Button>Profile</Button>
     </div>
   );
 }
